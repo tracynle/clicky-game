f56a2da4c01965ac8ba749818c20fe2c2232c711
Tracy Le <tnle9@asu.edu>
2019-04-20 23:53:25 -0700

    Fixed import for images to render

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import SculptureCard from "./components/Card";
 import sculptures from "./cards.json";
+import Wrapper from "./components/Wrapper";
 
 // create function that will shuffle (optional)
 // state to contain the data: paintings, score: 0, topScore: 0, 
@@ -31,18 +32,18 @@ class App extends Component {
           <h1>Clicky Game</h1>
           <h2>Click on an image to earn points, but don't click on any more than once! Good Luck!</h2>
         </header>
-        <main class="container">
+        <Wrapper>
           {this.state.sculptures.map(sculpture => (
             <SculptureCard 
+              key = {sculpture.id}
               image = {sculpture.image}
               artist = {sculpture.artist}
               title =  {sculpture.title}        
               year = {sculpture.year}  
             />
           ))}
-        </main>
+        </Wrapper>
       </div>
-
       
     );
   }
